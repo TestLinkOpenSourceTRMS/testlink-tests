6ea69908f9dea98f64c2fee7dfacd31ca91cf7b2
Francisco Mancardi <francisco.mancardi@gmail.com>
2013-07-22 23:44:01 +0200

    more examples

---
+++ rest-api/v1/curl/testproject/getOneTestProjectByID.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+# curl -i -X POST -u dev01:dev01 http://localhost/development/tlrepo/lib/api/rest/v1/testprojects
+# -i include headers
+# -X (HTTP) Specifies a custom request method to use when communicating with the HTTP server  
+# -u user:password
+# --data If you start the data with the letter @, the rest should be a file name to read the data from, 
+#        or - if you want curl to read the data from stdin. 
+#        The contents of the file must already be URL-encoded. 
+#        Multiple files can also be specified. 
+#        Posting data from a file named 'foobar' would thus be done with --data @foobar.
+#
+echo 'Testing TestLink REST API - GET /testprojects/374'
+curl -i -X GET  -u 524993143f974f97903f5565a783594b:dev01 http://localhost/development/tlrepo/lib/api/rest/v1/testprojects/374
