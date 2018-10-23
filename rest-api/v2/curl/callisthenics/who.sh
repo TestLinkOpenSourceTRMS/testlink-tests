226c4f0288d5c3d13e7dd70629dbf841e2ec6048
TestLink Jira <testlink.jira@gmail.com>
2018-10-23 15:04:25 +0200

    Started a new batch of test, this time aimed to version 2 (v2)

---
+++ rest-api/v2/curl/callisthenics/who.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# curl -i -X POST -u dev01:dev01 http://localhost/development/tlrepo/lib/api/rest/v1/testprojects
+# -i include headers
+# -X (HTTP) Specifies a custom request method to use when communicating with the HTTP server  
+# -u user:password
+# --data If you start the data with the letter @, the rest should be a file name to read the data from, 
+#        or - if you want curl to read the data from stdin. 
+#        The contents of the file must already be URL-encoded. 
+#        Multiple files can also be specified. 
+#        Posting data from a file named 'foobar' would thus be done with --data @foobar.
+#
+clear
+echo 'Testing TestLink REST API - POST /who'
+
+curl -i -H "Content-Type: application/json"  \
+     -u restadminapikey:followthewitherabbitneo \
+     http://localhost/development/github/testlink-code/lib/api/rest/v2/who
